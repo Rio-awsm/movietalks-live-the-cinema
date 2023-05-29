9b0a9f331ab5b465c1221ba1c7f350e6c224ac8d
Supriyo Saha <mm.saha1982@gmail.com>
2023-05-29 18:09:28 +0530

    hero banner created

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      Page not found
+    </div>
+  )
+}
+
+export default PageNotFound
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import homeSlice from "./homeSlice";
+
+export const Store = configureStore({
+    reducer: {
+        home: homeSlice,
+    },
+});
