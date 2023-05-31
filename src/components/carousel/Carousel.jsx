3dacac7a6ec4024e42b65b6395994274bc99133a
Supriyo Saha <mm.saha1982@gmail.com>
2023-05-31 14:15:12 +0530

    popular and top rated section added

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -13,7 +13,7 @@ import PosterFallback from "../../assets/images/no-poster.png";
 import Genres from "../genres/Genres";
 import "./style.scss";
 
-const Carousel = ({ data, loading }) => {
+const Carousel = ({ data, loading, endpoint }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
@@ -60,7 +60,7 @@ const Carousel = ({ data, loading }) => {
                 : PosterFallback;
               return (
                 <div key={item.id} className="carouselItem"
-                onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}
                 >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
