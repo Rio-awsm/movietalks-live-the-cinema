fad3db910e4a2e7865ab9f1e92c31881d791b9c4
Supriyo Saha <mm.saha1982@gmail.com>
2023-05-31 19:09:36 +0530

    Similar and recomendation added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,19 +4,26 @@ import "./style.scss";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideoSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 import useFetch from "../../hooks/useFetch";
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
-  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
-
-  return <div>
-    <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
-    <Cast  data={credits?.cast} loading={creditsLoading}/>
-    <VideosSection  data={data} loading={loading}/>
-  </div>;
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
+    </div>
+  );
 };
 
 export default Details;
