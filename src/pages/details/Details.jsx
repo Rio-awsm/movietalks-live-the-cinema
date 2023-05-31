5bf8321b99ab88e085131a91d6a0b6ff836c86e7
Supriyo Saha <mm.saha1982@gmail.com>
2023-05-31 19:05:03 +0530

    video section of movies added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import "./style.scss";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideoSection";
 
 import useFetch from "../../hooks/useFetch";
 const Details = () => {
@@ -14,6 +15,7 @@ const Details = () => {
   return <div>
     <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
     <Cast  data={credits?.cast} loading={creditsLoading}/>
+    <VideosSection  data={data} loading={loading}/>
   </div>;
 };
 
